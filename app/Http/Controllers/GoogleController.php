4527bd118bf7c04151144883c777ae5389b5e128
bachpx@pascaliaasia.com <bachpx@pascaliaasia.com>
2019-08-29 14:17:58 +0700

    a

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -13,20 +13,42 @@ class GoogleController extends Controller
 {
     public function getToken(){
         Log::info('received request');
+
+            $client = new \Google_Client();
+            $client->setAuthConfig(storage_path('app/tool/client_secret.json'));
+
+            $client->addScope('https://www.googleapis.com/auth/androidpublisher');
+
+            $redirect_uri = 'https://test.t4code.art/g_token';
+            $client->setRedirectUri($redirect_uri);
+
         if (isset($_GET['code'])) {
-            Log::info($_GET['code']);
-        } else {
+            $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+            $client->setAccessToken($token);
+            // store in the session also
+            $_SESSION['upload_token'] = $token;
+            
+        }
+// set the access token as part of the client
+        if (!empty($_SESSION['upload_token'])) {
             echo "<pre>";
-            print_r('vao roi');
+            print_r($_SESSION['upload_token']);
             echo "</pre>";
             exit();
-//            $client = new \Google_Client();
-//            $client->setAuthConfig(storage_path('app/tool/client_secret.json'));
-//
-//            $client->addScope(Google_Service_Drive::DRIVE);
-//
-//            $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
-//            $client->setRedirectUri($redirect_uri);
+//            $client->setAccessToken($_SESSION['upload_token']);
+//            if ($client->isAccessTokenExpired()) {
+//                unset($_SESSION['upload_token']);
+//            }
+        } else {
+            $authUrl = $client->createAuthUrl();
         }
+
+//            $service = new \Google_Service_AndroidPublisher($client);
+
+        $params = [
+            'url' => $authUrl
+        ];
+        return view('google.get_token', $params);
+
     }
 }
